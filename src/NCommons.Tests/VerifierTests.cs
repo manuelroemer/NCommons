180ac21bdaee8a58712a1491beb92995c51ebb12
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-09-26 14:25:36 +0200

    Added src with initial project files.

---
+++ src/NCommons.Tests/VerifierTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace NCommons.Tests
+{
+
+    public class VerifierTests
+    {
+
+        [Fact]
+        public static void VerifyNullThrows()
+        {
+            object nullObj = null;
+            var ex = Record.Exception(() => nullObj.VerifyNotNull());
+
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentNullException>(ex);
+        }
+
+    }
+
+}
