c68fe688d8b522ebe73d98b1f31384724c494fd7
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2018-09-26 14:55:11 +0200

    Added VerifyNull method. Added Strings Resource Dictionary.

--- src/NCommons.Tests/VerifierTests.cs
+++ src/NCommons.Tests/VerifierTests.cs
@@ -10,6 +10,16 @@ namespace NCommons.Tests
         [Fact]
         public static void VerifyNullThrows()
         {
+            var obj = new object();
+            var ex = Record.Exception(() => obj.VerifyNull());
+
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentException>(ex);
+        }
+
+        [Fact]
+        public static void VerifyNotNullThrows()
+        {
             object nullObj = null;
             var ex = Record.Exception(() => nullObj.VerifyNotNull());
 
--- src/NCommons/Verifier.cs
+++ src/NCommons/Verifier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NCommons.Resources;
+using System;
 using System.Diagnostics;
 
 namespace NCommons
@@ -6,19 +7,114 @@ namespace NCommons
     
     /// <summary>
     ///     Provides static extension methods in the form of
-    ///     <code>VerifyCondition(...)</code> which will throw exceptions if the condition
-    ///     is not met.
-    ///     scenarios.
+    ///     <code>Verify[Condition](...)</code>
+    ///     which will throw exceptions if the condition is not met.
     /// </summary>
     [DebuggerStepThrough]
     public static class Verifier
     {
 
+        #region Null
+
+        /// <summary>
+        ///     Ensures that the specified <paramref name="obj"/> is null,
+        ///     by throwing an <see cref="ArgumentException"/> if it is not null.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the object.
+        ///     Must be a reference type.
+        /// </typeparam>
+        /// <param name="obj">
+        ///     The object to be checked.
+        /// </param>
+        /// <returns>
+        ///     If no exception is thrown, this returns a reference to the specified
+        ///     <paramref name="obj"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///     Thrown if <paramref name="obj"/> is not null.
+        /// </exception>
+        public static T VerifyNull<T>(this T obj) where T : class
+        {
+            return VerifyNull(obj, null, Strings.Verifier_ObjectNotNullExMsg);
+        }
+
+        /// <summary>
+        ///     Ensures that the specified <paramref name="obj"/> is null,
+        ///     by throwing an <see cref="ArgumentException"/> if it is not null.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the object.
+        ///     Must be a reference type.
+        /// </typeparam>
+        /// <param name="obj">
+        ///     The object to be checked.
+        /// </param>
+        /// <param name="paramName">
+        ///     If the object to be checked is a parameter in a method, this can be set
+        ///     to the parameter's name.
+        ///     This results in a more detailed error message.
+        /// </param>
+        /// <returns>
+        ///     If no exception is thrown, this returns a reference to the specified
+        ///     <paramref name="obj"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///     Thrown if <paramref name="obj"/> is not null.
+        /// </exception>
+        public static T VerifyNull<T>(this T obj, string paramName) where T : class
+        {
+            return VerifyNull(
+                obj, 
+                paramName, 
+                string.Format(
+                    Strings.Verifier_ObjectNotNullExMsgWithParamName,
+                    paramName));
+        }
+
+        /// <summary>
+        ///     Ensures that the specified <paramref name="obj"/> is null,
+        ///     by throwing an <see cref="ArgumentException"/> if it is not null.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the object.
+        ///     Must be a reference type.
+        /// </typeparam>
+        /// <param name="obj">
+        ///     The object to be checked.
+        /// </param>
+        /// <param name="paramName">
+        ///     If the object to be checked is a parameter in a method, this can be set
+        ///     to the parameter's name.
+        ///     This results in a more detailed error message.
+        /// </param>
+        /// <param name="message">
+        ///     An error message which gets passed to the thrown exception.
+        /// </param>
+        /// <returns>
+        ///     If no exception is thrown, this returns a reference to the specified
+        ///     <paramref name="obj"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///     Thrown if <paramref name="obj"/> is not null.
+        /// </exception>
+        public static T VerifyNull<T>(this T obj, string paramName, string message) 
+            where T : class
+        {
+            if (obj != null)
+            {
+                throw new ArgumentException(message, paramName);
+            }
+            return obj;
+        }
+
+        #endregion
+
         #region NotNull
 
         /// <summary>
         ///     Ensures that the specified <paramref name="obj"/> is not null,
-        ///     by throwing a <see cref="ArgumentNullException"/> if it is null.
+        ///     by throwing an <see cref="ArgumentNullException"/> if it is null.
         /// </summary>
         /// <typeparam name="T">
         ///     The type of the object.
@@ -41,7 +137,7 @@ namespace NCommons
 
         /// <summary>
         ///     Ensures that the specified <paramref name="obj"/> is not null,
-        ///     by throwing a <see cref="ArgumentNullException"/> if it is null.
+        ///     by throwing an <see cref="ArgumentNullException"/> if it is null.
         /// </summary>
         /// <typeparam name="T">
         ///     The type of the object.
@@ -51,7 +147,7 @@ namespace NCommons
         ///     The object to be checked.
         /// </param>
         /// <param name="paramName">
-        ///     If the object to be checked is a parameter in a method, this field can be set
+        ///     If the object to be checked is a parameter in a method, this can be set
         ///     to the parameter's name.
         ///     This results in a more detailed error message.
         /// </param>
@@ -69,7 +165,7 @@ namespace NCommons
 
         /// <summary>
         ///     Ensures that the specified <paramref name="obj"/> is not null,
-        ///     by throwing a <see cref="ArgumentNullException"/> if it is null.
+        ///     by throwing an <see cref="ArgumentNullException"/> if it is null.
         /// </summary>
         /// <typeparam name="T">
         ///     The type of the object.
@@ -79,7 +175,7 @@ namespace NCommons
         ///     The object to be checked.
         /// </param>
         /// <param name="paramName">
-        ///     If the object to be checked is a parameter in a method, this field can be set
+        ///     If the object to be checked is a parameter in a method, this can be set
         ///     to the parameter's name.
         ///     This results in a more detailed error message.
         /// </param>
@@ -102,7 +198,7 @@ namespace NCommons
             }
             return obj;
         }
-
+        
         #endregion
 
     }
