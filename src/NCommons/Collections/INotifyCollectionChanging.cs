e82596d8a92f273a1c9e34c74c2c5b133d207942
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-03-31 16:45:52 +0200

    Added the skeleton for the PreviewingObservableCollection.

---
+++ src/NCommons/Collections/INotifyCollectionChanging.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Specialized;
+
+namespace NCommons.Collections
+{
+
+    /// <summary>
+    ///     Notifies listeners about when a collection is about to change, but before the change
+    ///     is actually going to happen.
+    /// </summary>
+    /// <remarks>
+    ///     This interface can be treated as the counterpart of the 
+    ///     <see cref="INotifyCollectionChanged"/> interface and should be implemented together
+    ///     with it.
+    ///     
+    ///     The <see cref="PreviewingObservableCollection"/> is a finished collection which
+    ///     implements both of these interfaces.
+    /// </remarks>
+    /// <seealso cref="INotifyCollectionChanged"/>
+    public interface INotifyCollectionChanging
+    {
+
+        /// <summary>
+        ///     Occurs when the collection is about to change, but before the change has actually
+        ///     happened.
+        /// </summary>
+        event NotifyCollectionChangedEventHandler CollectionChanging;
+
+    }
+
+}
---
+++ src/NCommons/Collections/PreviewingObservableCollection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace NCommons.Collections
+{
+
+    /// <summary>
+    ///     A dynamic data collection which provides notifications about when items are
+    ///     going to change and when they have actually changed.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    public class PreviewingObservableCollection<T> :
+        ObservableCollection<T>, INotifyCollectionChanging
+    {
+
+        /// <summary>
+        ///     Occurs when the collection is about to change, but before the change has actually
+        ///     happened.
+        /// </summary>
+        public event NotifyCollectionChangedEventHandler CollectionChanging;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PreviewingObservableCollection{T}"/> class.
+        /// </summary>
+        public PreviewingObservableCollection() { }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PreviewingObservableCollection{T}"/>
+        ///     class that contains elements copied from the specified <paramref name="collection"/>.
+        /// </summary>
+        /// <param name="collection">The collection from which the elements are copied.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="collection"/>
+        /// </exception>
+        public PreviewingObservableCollection(IEnumerable<T> collection)
+            : base(collection) { }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PreviewingObservableCollection{T}"/> 
+        ///     class that contains elements copied from the specified <paramref name="list"/>.
+        /// </summary>
+        /// <param name="list">The list from which the elements are copied.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="list"/>
+        /// </exception>
+        public PreviewingObservableCollection(List<T> list)
+            : base(list) { }
+
+        /// <summary>
+        ///     Raises the <see cref="INotifyCollectionChanging"/> event with the provided
+        ///     event args.
+        /// </summary>
+        /// <param name="e">The event args.</param>
+        protected virtual void OnCollectionChanging(NotifyCollectionChangedEventArgs e)
+        {
+            CollectionChanging?.Invoke(this, e);
+        }
+
+    }
+
+}
