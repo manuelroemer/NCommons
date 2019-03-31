58432beda26fa8b003540985476dab4912c87fb3
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-03-31 20:09:07 +0200

    Implemented reentrancy checks in the PreviewingObservableCollection.

--- src/NCommons/Collections/PreviewingObservableCollection.cs
+++ src/NCommons/Collections/PreviewingObservableCollection.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using NCommons.Collections.Resources;
 
 namespace NCommons.Collections
 {
+
+#pragma warning disable CA1001 // _reentrancyMonitor doesn't have to be disposed
 
     /// <summary>
     ///     A dynamic data collection which provides notifications about when items are
@@ -20,6 +23,8 @@ namespace NCommons.Collections
         ///     happened.
         /// </summary>
         public event NotifyCollectionChangedEventHandler CollectionChanging;
+
+        private readonly ReentrancyMonitor _reentrancyMonitor = new ReentrancyMonitor();
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="PreviewingObservableCollection{T}"/> class.
@@ -48,6 +53,14 @@ namespace NCommons.Collections
         public PreviewingObservableCollection(List<T> list)
             : base(list) { }
 
+
+
+        //
+        // Note: In the following, calls to BlockCollectionChangingReentrancy are made.
+        //       Calling this method has the same effect as BlockReentrancy which, for perf.
+        //       reasons, does not have to be called by us.
+        //
+
         /// <summary>
         ///     Inserts the <paramref name="item"/> into the collection at the specified <paramref name="index"/>.
         /// </summary>
@@ -55,7 +68,8 @@ namespace NCommons.Collections
         /// <param name="item">The item to be inserted.</param>
         protected override void InsertItem(int index, T item)
         {
-            using (BlockReentrancy())
+            CheckCollectionChangingReentrancy();
+            using (BlockCollectionChangingReentrancy())
             {
                 OnCollectionChanging(new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Add,
@@ -71,7 +85,8 @@ namespace NCommons.Collections
         /// </summary>
         protected override void ClearItems()
         {
-            using (BlockReentrancy())
+            CheckCollectionChangingReentrancy();
+            using (BlockCollectionChangingReentrancy())
             {
                 OnCollectionChanging(new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Reset
@@ -86,7 +101,8 @@ namespace NCommons.Collections
         /// <param name="index">The zero-based index of the item to be removed.</param>
         protected override void RemoveItem(int index)
         {
-            using (BlockReentrancy())
+            CheckCollectionChangingReentrancy();
+            using (BlockCollectionChangingReentrancy())
             {
                 OnCollectionChanging(new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Remove,
@@ -104,7 +120,8 @@ namespace NCommons.Collections
         /// <param name="item">The item with which to replace the old item.</param>
         protected override void SetItem(int index, T item)
         {
-            using (BlockReentrancy())
+            CheckCollectionChangingReentrancy();
+            using (BlockCollectionChangingReentrancy())
             {
                 OnCollectionChanging(new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Replace,
@@ -123,7 +140,8 @@ namespace NCommons.Collections
         /// <param name="newIndex">The zero-based index to which the item should be moved.</param>
         protected override void MoveItem(int oldIndex, int newIndex)
         {
-            using (BlockReentrancy())
+            CheckCollectionChangingReentrancy();
+            using (BlockCollectionChangingReentrancy())
             {
                 OnCollectionChanging(new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Move,
@@ -145,6 +163,68 @@ namespace NCommons.Collections
             CollectionChanging?.Invoke(this, e);
         }
 
+        /// <summary>
+        ///     Ensures that any action which modifies this collection throws an
+        ///     <see cref="InvalidOperationException"/> while the returned 
+        ///     <see cref="IDisposable"/> has not been disposed.
+        ///     
+        ///     See remarks for details.
+        /// </summary>
+        /// <returns>
+        ///     An <see cref="IDisposable"/> which is used to scope the lifetime of the
+        ///     reentrancy block.
+        /// </returns>
+        /// <remarks>
+        ///     The only exception to this rule is if <see cref="CollectionChanging"/> has 1 or less
+        ///     subscribers.
+        ///     
+        ///     This method has the same functionality as
+        ///     <see cref="ObservableCollection{T}.BlockReentrancy"/>, but only for the
+        ///     <see cref="CollectionChanging"/> event.
+        ///     
+        ///     Deriving classes must call <see cref="CheckCollectionChangingReentrancy"/> for 
+        ///     throwing the exception.
+        /// </remarks>
+        /// <see cref="CheckCollectionChangingReentrancy"/>
+        /// <seealso cref="ObservableCollection{T}.BlockReentrancy"/>
+        protected IDisposable BlockCollectionChangingReentrancy()
+        {
+            _reentrancyMonitor.Enter();
+            return _reentrancyMonitor;
+        }
+
+        /// <summary>
+        ///     Throws an <see cref="InvalidOperationException"/> if the conditions of
+        ///     <see cref="BlockCollectionChangingReentrancy"/> are met.
+        /// </summary>
+        /// <seealso cref="BlockCollectionChangingReentrancy"/>
+        /// <seealso cref="ObservableCollection{T}.CheckReentrancy"/>
+        protected void CheckCollectionChangingReentrancy()
+        {
+            if (_reentrancyMonitor.IsBusy &&
+                CollectionChanging != null &&
+                CollectionChanging.GetInvocationList().Length > 1)
+            {
+                throw new InvalidOperationException(
+                    ExceptionStrings.PreviewingObservableCollection_CollectionChangingReentrancy
+                );
+            }
+        }
+
+        /// <summary>
+        ///     Used for reentrancy checks. Follows the implementation of the
+        ///     ObservableCollection{T}. See the reference source for details.
+        /// </summary>
+        private class ReentrancyMonitor : IDisposable
+        {
+            private int _busyCount;
+            public bool IsBusy => _busyCount > 0;
+            public void Enter() => _busyCount++;
+            public void Dispose() => _busyCount--;
+        }
+
     }
+
+#pragma warning restore CA1001 // _reentrancyMonitor doesn'T have to be disposed
 
 }
