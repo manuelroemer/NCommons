bd00dc90f7fe2a75b675d0302e39d683247c890b
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-06-15 22:09:38 +0200

    Updated PreviewingObservableCollection test structure.

---
+++ src/NCommons.Collections.Tests/PreviewingObservableCollection/PreviewingObservableCollectionTestBase.cs
@@ -0,0 +1,25 @@
+﻿namespace NCommons.Collections.Tests.PreviewingObservableCollection
+{
+    using System;
+    using System.Collections.Specialized;
+    using static Xunit.Assert;
+    using Assert = NCommons.Tests.AssertEx;
+
+    public class PreviewingObservableCollectionTestBase
+    {
+
+        protected static RaisedEvent<NotifyCollectionChangedEventArgs> AssertRaisesCollectionChanging<T>(
+            PreviewingObservableCollection<T> collection,
+            Action testCode)
+        {
+            return Assert.Raises<NotifyCollectionChangingEventHandler, NotifyCollectionChangedEventArgs>(
+                ev => new NotifyCollectionChangingEventHandler(ev),
+                handler => collection.CollectionChanging += handler,
+                handler => collection.CollectionChanging -= handler,
+                testCode
+            );
+        }
+
+    }
+
+}
