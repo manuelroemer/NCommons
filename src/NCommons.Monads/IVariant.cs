ec231b56b4c4ec25daa98a8337ed145d9295843c
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-06-16 16:04:34 +0200

    Added first variant struct.

---
+++ src/NCommons.Monads/IVariant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NCommons.Monads
+{
+
+    /// <summary>
+    ///     Declares shared members of the generic variant structs, thus enabling extension methods
+    ///     for shared implementation logic.
+    /// </summary>
+    internal interface IVariant
+    {
+
+        /// <summary>
+        ///     Gets or sets the value which the variant holds as an <see cref="object"/>.
+        ///     This may be <see langword="null"/>, depending on the value with which the variant
+        ///     was initialized.
+        ///     
+        ///     If the variant is empty this is <see langword="null"/>.
+        /// </summary>
+        object? Value { get; }
+
+        /// <summary>
+        ///     Gets the type of the variant, i.e. the enum value which defines what value it holds.
+        /// </summary>
+        VariantType Type { get; }
+
+        /// <summary>
+        ///     Gets the types of the variant's generic type parameters (in order).
+        ///     This is used for building appropriate exception message strings.
+        /// </summary>
+        Type[] GenericValueTypes { get; }
+
+    }
+
+}
